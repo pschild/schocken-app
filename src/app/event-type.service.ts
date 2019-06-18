c6c54da824a3049d5111c8ee094ff3f0d3432197
Schild, Philippe <philippe.schild@adesso.de>
2019-06-18 21:15:14 +0200

    changes

--- src/app/event-type.service.ts
+++ src/app/event-type.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { PouchDbService, GetResponse, PutResponse } from './pouchDb.service';
-import { EntityType, EventType } from './interfaces';
+import { PouchDbService, GetResponse, PutResponse, FindResponse } from './pouchDb.service';
+import { EntityType, EventType, EventTypeContext } from './interfaces';
 import { from, Observable } from 'rxjs';
 
 @Injectable({
@@ -10,19 +10,21 @@ export class EventTypeService {
 
   constructor(private pouchDbService: PouchDbService) { }
 
-  getAll(): Observable<GetResponse<EventType>> {
-    return from(this.pouchDbService.getAll('eventType'));
+  getAllByContext(context: EventTypeContext): Observable<FindResponse<EventType>> {
+    const selector = { context, type: EntityType.EVENT_TYPE };
+    return from(this.pouchDbService.findWithPlugin(selector));
   }
 
   getById(id: string): Observable<EventType> {
     return from(this.pouchDbService.getOne(id));
   }
 
-  create(name: string, penalty?: number): Observable<PutResponse> {
+  create(name: string, context: EventTypeContext, penalty?: number): Observable<PutResponse> {
     const eventType: EventType = {
       _id: this.pouchDbService.generateId('eventType'),
       type: EntityType.EVENT_TYPE,
       name,
+      context,
       penalty
     };
     return from(this.pouchDbService.create(eventType));
