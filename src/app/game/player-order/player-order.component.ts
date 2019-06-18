e9d7f0c5bd126bcde61dd94053015537ca07facd
Schild, Philippe <philippe.schild@adesso.de>
2019-06-18 20:28:32 +0200

    removed duplicated code

--- src/app/game/player-order/player-order.component.ts
+++ src/app/game/player-order/player-order.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { from, Observable, combineLatest } from 'rxjs';
+import { Observable } from 'rxjs';
 import { RoundService } from 'src/app/round.service';
 import { PutResponse, GetResponse } from 'src/app/pouchDb.service';
 import { PlayerService } from 'src/app/player.service';
 import { map, switchMap } from 'rxjs/operators';
-import { Player, Game, Round } from 'src/app/interfaces';
+import { Player, Round } from 'src/app/interfaces';
 import { GameService } from 'src/app/game.service';
 
 @Component({
@@ -17,7 +17,6 @@ export class PlayerOrderComponent implements OnInit {
 
   allPlayers$: Observable<Array<Player>>;
   participatingPlayerIds: Array<{playerId: string; inGame: boolean}> = [];
-  returnToGame: boolean;
 
   constructor(private roundService: RoundService, private gameService: GameService, private playerService: PlayerService, private router: Router, private route: ActivatedRoute) { }
 
@@ -36,12 +35,6 @@ export class PlayerOrderComponent implements OnInit {
     ).subscribe((participatingPlayerIds: Array<{playerId: string; inGame: boolean}>) => {
       this.participatingPlayerIds = participatingPlayerIds || [];
     });
-
-    this.route.params.pipe(
-      map(params => !!params.returnToGame)
-    ).subscribe((returnToGame: boolean) => {
-      this.returnToGame = returnToGame;
-    });
   }
 
   toggleParticipations(playerId: string, event) {
@@ -56,7 +49,7 @@ export class PlayerOrderComponent implements OnInit {
     return this.participatingPlayerIds && !!this.participatingPlayerIds.find(item => item.playerId === playerId);
   }
 
-  startGame() {
+  handleSaveClicked() {
     const currentGameId = this.route.snapshot.paramMap.get('gameId');
     const currentRoundId = this.route.snapshot.paramMap.get('roundId');
     this.roundService.update(currentRoundId, { participatingPlayerIds: this.participatingPlayerIds })
@@ -65,19 +58,6 @@ export class PlayerOrderComponent implements OnInit {
           this.router.navigate(['/game', currentGameId]);
         } else {
           throw new Error(`Participating players could not be set on round`);
-        }
-      });
-  }
-
-  backToGame() {
-    const currentGameId = this.route.snapshot.paramMap.get('gameId');
-    const currentRoundId = this.route.snapshot.paramMap.get('roundId');
-    this.roundService.update(currentRoundId, { participatingPlayerIds: this.participatingPlayerIds })
-      .subscribe((gameResponse: PutResponse) => {
-        if (gameResponse.ok === true) {
-          this.router.navigate(['/game', currentGameId]);
-        } else {
-          throw new Error(`There was an error`);
         }
       });
   }
