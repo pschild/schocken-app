0c1149a69e07b0e4121f436b2281d36f800e0b15
Schild, Philippe <Philippe.Schild@adesso.de>
2019-07-19 08:28:11 +0200

    navigation closes on navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-navigation',
@@ -10,11 +12,17 @@ import { map } from 'rxjs/operators';
 })
 export class NavigationComponent {
 
+  @ViewChild('drawer', { static: true }) drawer: MatSidenav;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, router: Router) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(_ => this.drawer.close());
+  }
 
 }
