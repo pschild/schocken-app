36e0cb0bbde4a3799a99d70563e698ce5f37fdde
Schild, Philippe <Philippe.Schild@adesso.de>
2020-03-12 21:53:36 +0100

    styling and bugfixes

--- projects/hop-basic-components/src/lib/odometer/odometer.component.ts
+++ projects/hop-basic-components/src/lib/odometer/odometer.component.ts
@@ -14,15 +14,21 @@ export class OdometerComponent implements OnInit {
   value$: Observable<number>;
 
   @Input() countTo: number;
+  @Input() precision: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (!this.precision) {
+      this.precision = 0;
+    }
+
     this.value$ = timer(0, this.COUNT_INTERVAL_MS).pipe(
       map((currentValue: number) => {
         const newValue = currentValue * (this.countTo / 100);
         return newValue > this.countTo ? this.countTo : newValue;
       }),
+      map((currentValue: number) => +currentValue.toFixed(this.precision)),
       takeWhile((currentValue: number) => currentValue <= this.countTo)
     );
   }
