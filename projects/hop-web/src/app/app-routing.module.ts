5704b01e529e07f79cbfe4bd1ec775e61c44a10a
Philippe Schild <Philippe.Schild@adesso.de>
2023-06-07 07:21:10 +0200

    Update to Angular 15

--- projects/hop-web/src/app/app-routing.module.ts
+++ projects/hop-web/src/app/app-routing.module.ts
@@ -51,9 +51,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    // preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
-})],
+    // preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
